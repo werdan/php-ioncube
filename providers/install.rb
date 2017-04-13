47a5efc021fac6e5a6efe6ee45f1fcfcf1ef3681
Edgaras Lukosevicius <edgaras.lukosevicius@gmail.com>
2017-04-13 18:51:46 +0300

    Update cookbook for Chef 13

--- providers/install.rb
+++ providers/install.rb
@@ -1,29 +1,25 @@
-use_inline_resources if defined?(use_inline_resources)
-
 action :install do
+  arch_string = case node['kernel']['machine']
+                when 'x86_64'
+                  'x86-64'
+                when /i[36]86/
+                  'x86'
+                else
+                  node['kernel']['machine']
+                end
 
-  case run_context.node[:kernel][:machine]
-  when 'x86_64'
-    arch_string = 'x86-64'
-  when /i[36]86/
-    arch_string = 'x86'
-  else
-    arch_string = run_context.node[:kernel][:machine]
-  end
-
-  Chef::Log.debug("case selected #{arch_string}")
+  Chef::Log.debug("Selected #{arch_string} arch")
 
   remote_file "/usr/local/src/ioncube_loaders_lin_#{arch_string}.tar.gz" do
     source "http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_lin_#{arch_string}.tar.gz"
-    mode "0644"
     action :create_if_missing
-    notifies :run, "script[extract_ioncube_php]", :immediately
+    notifies :run, 'script[Extract_ioncube_php]', :immediately
   end
 
-  script "extract_ioncube_php" do
-    interpreter "bash"
-    user "root"
-    cwd "/usr/local/src/"
+  script 'Extract_ioncube_php' do
+    interpreter 'bash'
+    user 'root'
+    cwd '/usr/local/src/'
     action :nothing
     code <<-EOH
     tar xvfz /usr/local/src/ioncube_loaders_lin_#{arch_string}.tar.gz
@@ -31,29 +27,24 @@ action :install do
     EOH
   end
 
-  ruby_block "determine php version" do
+  ruby_block 'Determine php version' do
     block do
-      php_version_output = `php --version`
+      php_version_output = shell_out!('php --version').stdout
       php_version = php_version_output.match(/PHP ([0-9]+\.[0-9]+)\.[0-9]+/)[1]
-      Chef::Log.info("detected PHP version #{php_version}")
-      ioncube_file_resource = run_context.resource_collection.find(:file => "#{run_context.node[:php][:ext_conf_dir]}/ioncube.ini")
+      Chef::Log.info("Detected PHP version #{php_version}")
+      ioncube_file_resource = resource_collection.find(file: "#{node['php']['ext_conf_dir']}/ioncube.ini")
       ioncube_file_resource.content "; priority=00\nzend_extension=/usr/local/ioncube/ioncube_loader_lin_" + php_version + ".so\n"
     end
-    only_if { run_context.node[:php_ioncube][:version] == '' }
+    only_if { node['php_ioncube']['version'] == '' }
   end
 
-  file "#{run_context.node[:php][:ext_conf_dir]}/ioncube.ini" do
-    content "; priority=00\nzend_extension=/usr/local/ioncube/ioncube_loader_lin_" + run_context.node[:php_ioncube][:version] + ".so\n"  # dynamically defined during convergence in above ruby_block
-    owner "root"
-    group "root"
-    mode "0644"
-    action :create
+  file "#{node['php']['ext_conf_dir']}/ioncube.ini" do
+    content "; priority=00\nzend_extension=/usr/local/ioncube/ioncube_loader_lin_" + node['php_ioncube']['version'] + ".so\n"
   end
 
   if new_resource.updated_by_last_action?
-    Chef::Log.info("new_resource.updated_by_last_action is true")
+    Chef::Log.info('new_resource.updated_by_last_action is true')
   else
-    Chef::Log.info("new_resource.updated_by_last_action is false")
+    Chef::Log.info('new_resource.updated_by_last_action is false')
   end
-
 end
